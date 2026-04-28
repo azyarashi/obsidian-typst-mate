9fc819309df49696dc4c1c35c5374064de581c5a
azyarashi <me@azyarashi.com>
2026-04-28 14:29:22 +0900

    条件を修正

--- src/editor/shared/extensions/Tooltip/extension.tsx
+++ src/editor/shared/extensions/Tooltip/extension.tsx
@@ -1,7 +1,7 @@
-import { SyntaxMode } from '@typstmate/typst-syntax';
 import { type EditorView, hoverTooltip } from '@codemirror/view';
 import { render } from 'preact';
 import { typstManager } from '@/libs';
+import { RenderingEngine } from '@/libs/processor';
 import { getActiveRegion } from '../../utils/core';
 
 import './Tooltip.css';
@@ -9,15 +9,13 @@ import './Tooltip.css';
 export const hoverExtension = hoverTooltip(async (view: EditorView, pos: number, side: number) => {
   const region = getActiveRegion(view);
   if (!region?.tree) return null;
-
-  const mode = region.activeMode ?? region.mode;
-  if (![SyntaxMode.Math, SyntaxMode.Code].includes(mode)) return null;
+  if (region.processor && region.processor.renderingEngine === RenderingEngine.MathJax) return null;
 
   const innerFrom = region.from + region.skip;
   const code = view.state.sliceDoc(innerFrom, region.to);
 
   const wasmPos = pos - innerFrom;
-  if (wasmPos < 0 || wasmPos > code.length) return null;
+  if (wasmPos < 0 || code.length < wasmPos) return null;
 
   try {
     const tooltip = await typstManager.wasm.tooltip(wasmPos, code, side === 1);
