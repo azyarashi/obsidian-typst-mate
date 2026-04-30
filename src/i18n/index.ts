24d0af8ef621212bdaf574c8a95bc9d26b5fa5eb
azyarashi <me@azyarashi.com>
2026-04-30 12:20:25 +0900

    jsonc に変更

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,5 +1,5 @@
-import en from '@resources/locales/en.json';
-import ja from '@resources/locales/ja.json';
+import en from '@resources/locales/en.jsonc';
+import ja from '@resources/locales/ja.jsonc';
 import { I18n } from '@typst-mate/i18n';
 
 const resources = {
